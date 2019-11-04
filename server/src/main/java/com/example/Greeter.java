83d8b3ba271e09b03c133a9b388f905e7935eb68
Illumias <oliver.varnild@live.dk>
2019-11-04 09:17:47 +0100

    c

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,12 +15,12 @@ public class Greeter {
 
   }
 
-    /**
-     * This method greet the user.
-     * @param someone
-     * @return
-     */
-    public final String greet(final String someone) {
+  /**
+   * This method greet the user.
+   * @param someone
+   * @return
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This is the standard package
+ */
+package com.example;
